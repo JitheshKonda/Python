9f2040f947df033fd8f3d775549b9290d1e99854
Konda Vinay Prasanna Jitesh <kondavinay52@gmail.com>
2026-01-10 18:38:28 +0300

    Create function.py

---
+++ function.py
@@ -0,0 +1,17 @@
+#Defininig and calling  a function 
+def add(a, b):
+    return a + b
+
+print(add(2, 3))
+
+#Function with parameters
+def greet(name):
+    print("Hello", name)
+
+greet("Sam")
+
+#Example programs
+def lucky(*name)
+    print("hi",name)
+lakshmmi(name='ram',age=29)
+
