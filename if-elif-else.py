e5cc74fb6523206932265e53fcf581e680c00167
Konda Vinay Prasanna Jitesh <kondavinay52@gmail.com>
2026-07-24 20:57:29 +0530

    Create if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,13 @@
+marks = 85
+if marks >= 90:
+    print("Grade A+")
+elif marks >= 75:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 45:
+   print("Grade C")
+elif marks >= 35:
+  print("Grade D")
+else:
+  print("Fail")
